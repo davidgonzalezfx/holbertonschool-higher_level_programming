1a67b862e19b9952bc6c9c244ef42c65264beff3
davidgonzalezfx <dvdramos16@gmail.com>
2020-04-27 11:26:55 -0500

    [Feature #1] search infinite loop on linkedlists”
    -> Using floyd algorithm
    -> Delete junk file (main.py and 10-main.c)

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * check_cycle - search infinite loop on linkedlists
+ * @list: linkedlist head *
+ * Return: 1 if cycle found, 0 otherwise
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *forwd = list;
+	listint_t *back = list;
+
+	if (!list || !list->next)
+		return (0);
+
+	for (; forwd != NULL;)
+	{
+		back = back->next;
+		forwd = forwd->next->next;
+
+		if (back == forwd)
+			return (1);
+	}
+	return (0);
+}
