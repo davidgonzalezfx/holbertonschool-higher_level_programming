e4fbec18e3eac94c2fd4da418fafbf2d09b9ac93
davidgonzalezfx <dvdramos16@gmail.com>
2020-04-28 17:07:24 -0500

    [Feature #1] Insert node in order mode to linkedlist

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,33 @@
+include "lists.h"
+/**
+ * insert_node - Insert node in order mode to linkedlist
+ * @head: head
+ * @number: num to be added
+ * Return: the head of node
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new = malloc(sizeof(listint_t));
+	listint_t *actual;
+
+	if (!new || !head)
+		return (NULL);
+
+	new->n = number;
+	actual = *head;
+
+	if (number < actual->n)
+	{
+		new->next = actual;
+		*head = new;
+		return (*head);
+	}
+
+	while ((actual->next)->n < number)
+		actual = actual->next;
+
+	new->next = (actual->next)->next;
+	actual->next = new;
+
+	return (*head);
+}
