a32819a3bead63c33c998a618bc56713e896d80a
davidgonzalezfx <dvdramos16@gmail.com>
2020-04-28 13:50:56 -0500

    [Fix #1] Fix last num assignment

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = number % 10
 if number < 0:
-		last = (number * -1) % 10 * -1
+    last = (number * -1) % 10 * -1
+else:
+    last = number % 10
 print('Last digit of {} is {} and is '.format(number, last), end="")
 if last > 5:
     print('greater than 5')
