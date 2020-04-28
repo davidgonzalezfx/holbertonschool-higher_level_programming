f9928f59a1ff87331b9ea08edb785ed8186fd31b
davidgonzalezfx <dvdramos16@gmail.com>
2020-04-28 15:38:31 -0500

    [Fix #1] j range fixed

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-listint_t *insert_node(listint_t **head, int number);
+for i in range(0, 9):
+    for j in range(1, 10):
+        if i < j:
+            if i == 8:
+                print('{}{}'.format(i, j))
+            else:
+                print('{}{}, '.format(i, j), end='')
