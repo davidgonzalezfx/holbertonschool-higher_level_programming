7aba84b7f5e71b1865d9019dfcbb13fbe5194889
davidgonzalezfx <dvdramos16@gmail.com>
2020-04-30 11:05:22 -0500

    [Feature] Imports all functions from the file calculator_1.py and handles basics operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv
+if __name__ == '__main__':
+    if len(argv) != 4:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
+    operators = ['+', '-', '*', '/']
+    operator = argv[2]
+    if operator in operators:
+        a = int(argv[1])
+        b = int(argv[3])
+        if operator is '+':
+            print('{} + {} = {}'.format(a, b, add(a, b)))
+        elif operator is '-':
+            print('{} - {} = {}'.format(a, b, sub(a, b)))
+        elif operator is '*':
+            print('{} * {} = {}'.format(a, b, mul(a, b)))
+        elif operator is '/':
+            print('{} / {} = {}'.format(a, b, div(a, b)))
+    else:
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
