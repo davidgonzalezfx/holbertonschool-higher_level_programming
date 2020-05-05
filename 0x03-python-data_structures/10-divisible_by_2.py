67ee21c277873a1daf2152028052f781a3b29820
davidgonzalezfx <dvdramos16@gmail.com>
2020-05-05 15:42:15 -0500

    Fix - Using in range

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     by_2 = my_list[:]
-    for el in by_2:
-        if el % 2 is 0:
-            el = True
+    for idx in range(0, len(by_2)):
+        if by_2[idx] % 2 is 0:
+            by_2[idx] = True
         else:
-            el = False
+            by_2[idx] = False
     return by_2
