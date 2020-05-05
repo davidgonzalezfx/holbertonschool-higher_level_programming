79a02c9e29e6ac5581155449850da39e25c264a7
davidgonzalezfx <dvdramos16@gmail.com>
2020-05-05 14:24:19 -0500

    Fix - Change assignment

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     by_2 = my_list[:]
     for el in by_2:
         if el % 2 is 0:
-            by_2[el] = True
+            el = True
         else:
-            by_2[el] = False
+            el = False
     return by_2
