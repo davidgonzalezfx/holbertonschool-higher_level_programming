6c7391eeb9ca439294b17a97175e32af20294637
davidgonzalezfx <dvdramos16@gmail.com>
2020-05-05 13:51:24 -0500

    Fix - No copy tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,17 +2,13 @@
 def add_tuple(tuple_a=(), tuple_b=()):
 
     if len(tuple_a) is 0:
-        cp_a = (0, 0)
+        tuple_a = (0, 0)
     elif len(tuple_a) is 1:
-        cp_a = (tuple_a[0], 0)
-    elif len(tuple_a) > 1:
-        cp_a = (tuple_a[0], tuple_a[1])
+        tuple_a = (tuple_a[0], 0)
 
     if len(tuple_b) is 0:
-        cp_b = (0, 0)
+        tuple_b = (0, 0)
     elif len(tuple_b) is 1:
-        cp_b = (tuple_b[0], 0)
-    elif len(tuple_a) > 1:
-        cp_b = (tuple_b[0], tuple_b[1])
+        tuple_b = (tuple_b[0], 0)
 
-    return (cp_a[0] + cp_b[0], cp_a[1] + cp_b[1])
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
