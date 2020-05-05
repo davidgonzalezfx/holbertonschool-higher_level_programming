e2a4971de1ccdb9f0dd56b0696dacc5cbbf718a6
davidgonzalezfx <dvdramos16@gmail.com>
2020-05-05 14:15:54 -0500

    Fix - Swap conditions

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,8 +2,8 @@
 def divisible_by_2(my_list=[]):
     by_2 = list(my_list)
     for el in by_2:
-        if not el % 2 is 0:
-            by_2[el] = False
-        else:
+        if el % 2 is 0:
             by_2[el] = True
+        else:
+            by_2[el] = False
     return by_2
