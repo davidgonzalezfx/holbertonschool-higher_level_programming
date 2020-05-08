2d584551c1c5a353c9b28a3c3c57c4c05fab505a
davidgonzalezfx <dvdramos16@gmail.com>
2020-05-07 20:51:45 -0500

    Fix - Use dict() to get the new size of a_dictonary

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for a_key, a_value in a_dictionary.items():
+    for a_key, a_value in dict(a_dictionary).items():
         if value is a_value:
             a_dictionary.pop(a_key)
     return a_dictionary
