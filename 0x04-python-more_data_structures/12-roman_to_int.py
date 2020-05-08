320cf18bc103adb400943a71f917b04834f6d42c
davidgonzalezfx <dvdramos16@gmail.com>
2020-05-07 19:31:35 -0500

    Feature - Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,29 @@
 #!/usr/bin/python3
-def weight_average(my_list=[]):
+def roman_to_int(roman_string):
+    if (roman_string is None) or (type(roman_string) != str):
+            return 0
+
+    roman = 0
+    nums = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    limit = len(roman_string)
+    for ch in roman_string:
+        for key, value in nums.items():
+            if ch is key:
+                if roman_string.index(ch) == limit - 1:
+                    roman += value
+                    break
+                nxt = roman_string[roman_string.index(ch) + 1]
+                if value >= nums.get(nxt):
+                    roman += value
+                else:
+                    roman -= value
+    return roman
