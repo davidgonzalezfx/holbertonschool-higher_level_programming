f428c5b89ed8ad829136490dca28bc7cf7d13d40
davidgonzalezfx <dvdramos16@gmail.com>
2020-05-07 20:51:06 -0500

    Feature - deletes keys with a specific value in a dictionary

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
-void print_python_list(PyObject *p);
+def complex_delete(a_dictionary, value):
+    for a_key, a_value in a_dictionary.items():
+        if value is a_value:
+            a_dictionary.pop(a_key)
+    return a_dictionary
