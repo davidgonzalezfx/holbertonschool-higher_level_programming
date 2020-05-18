6beb40606ed6ca38fe0168932ee37f2e2d198be6
davidgonzalezfx <dvdramos16@gmail.com>
2020-05-18 11:08:34 -0500

    Feature - prints x elements of a list in safe mode

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    if my_list and x > 0:
+        try:
+            for idx, el in enumerate(my_list):
+                print('{}'.format(el), end='')
+            print()
+            return idx
+        except:
+            print()
+            return idx
+    else:
+        return 0
