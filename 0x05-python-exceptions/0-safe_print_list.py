e9e378bf1aa375b430bfb0bba1797f4fe1207738
davidgonzalezfx <dvdramos16@gmail.com>
2020-05-18 12:05:51 -0500

    Fix - Remove if condition

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if my_list and x > 0:
-        try:
-            for idx in range(x):
-                print('{}'.format(my_list[idx]), end='')
-            print()
-            return idx + 1
-        except IndexError:
-            print()
-            return idx
-    else:
-        return 0
+    try:
+        for idx in range(x):
+            print('{}'.format(my_list[idx]), end='')
+        print()
+        return idx + 1
+    except IndexError:
+        print()
+        return idx
