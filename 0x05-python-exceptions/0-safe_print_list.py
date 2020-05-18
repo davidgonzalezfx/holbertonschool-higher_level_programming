c86eb3b2a7a58ebd7bb82e3c5b67788e7314f5cd
davidgonzalezfx <dvdramos16@gmail.com>
2020-05-18 12:07:32 -0500

    Fix - Return x if success

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
         for idx in range(x):
             print('{}'.format(my_list[idx]), end='')
         print()
-        return idx + 1
+        return x
     except IndexError:
         print()
         return idx
