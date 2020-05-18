f3f0bb5d3c591d865c901590b5026cb629dc9575
davidgonzalezfx <dvdramos16@gmail.com>
2020-05-18 11:48:46 -0500

    Feature - prints an integer and errors in stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
+    from sys import stderr
+    try:
+        print('{:d}'.format(value))
+        return True
+    except Exception as error:
+        print('Exception: {:s}'.format(str(error)), file=stderr)
+        return False
