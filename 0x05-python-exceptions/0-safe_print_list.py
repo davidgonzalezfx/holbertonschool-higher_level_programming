a960b927b175252c8ad4d2a6d0040ace57d86471
davidgonzalezfx <dvdramos16@gmail.com>
2020-05-18 12:04:01 -0500

    Fix - Fix exception return

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,6 @@ def safe_print_list(my_list=[], x=0):
             return idx + 1
         except IndexError:
             print()
-            return idx - 1
+            return idx
     else:
         return 0
