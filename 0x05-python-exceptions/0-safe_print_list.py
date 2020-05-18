438dc947d54a56765692befb0e44fbcafa750b70
davidgonzalezfx <dvdramos16@gmail.com>
2020-05-18 12:03:04 -0500

    Fix - change loop and fix pep8 warning in exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,12 +2,12 @@
 def safe_print_list(my_list=[], x=0):
     if my_list and x > 0:
         try:
-            for idx, el in enumerate(my_list):
-                print('{}'.format(el), end='')
+            for idx in range(x):
+                print('{}'.format(my_list[idx]), end='')
             print()
-            return idx
-        except:
+            return idx + 1
+        except IndexError:
             print()
-            return idx
+            return idx - 1
     else:
         return 0
