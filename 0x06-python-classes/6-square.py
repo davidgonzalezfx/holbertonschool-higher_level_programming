169159570deb3b112359b9ee95c89ceeb5ad3874
davidgonzalezfx <dvdramos16@gmail.com>
2020-05-19 13:11:51 -0500

    Fix - PEP8 code style

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -22,7 +22,9 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if type(value) != tuple or len(value) != 2 or type(value[0]) != int or type(value[1]) != int:
+        if type(value) != tuple or len(value) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if type(value[0]) != int or type(value[1]) != int:
             raise TypeError('position must be a tuple of 2 positive integers')
         if value[0] < 0 or value[1] < 0:
             raise ValueError('position must be a tuple of 2 positive integers')
