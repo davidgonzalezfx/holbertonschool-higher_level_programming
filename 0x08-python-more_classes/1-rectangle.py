a7b5dcf586fc297ee479cae9664f22bb8546a28f
davidgonzalezfx <dvdramos16@gmail.com>
2020-05-25 12:42:33 -0500

    Feature - Rectangle that defines a rectangle by:
    -> Private instance attribute: width (int)
    -> Private instance attribute: height (int)
    -> Instantiation with optional width and heigh

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,1 +1,41 @@
 #!/usr/bin/python3
+class Rectangle:
+    """
+    Rectangle that defines a rectangle by:
+    Private instance attribute: width (int)
+    Private instance attribute: height (int)
+    Instantiation with optional width and heigh
+    """
+
+    def __init__(self, width=0, height=0):
+        """ Constructor method """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ getter width property """
+        return self.width
+
+    @property
+    def height(self):
+        """ getter height property """
+        return self.height
+
+    @width.setter
+    def width(self, value):
+        """ setter width property """
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """ setter height property """
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
