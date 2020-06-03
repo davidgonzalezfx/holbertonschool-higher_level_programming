8ebfc658e4b6ce5774a35e2338cf658caa84ec93
davidgonzalezfx <dvdramos16@gmail.com>
2020-06-03 13:15:53 -0500

    Feature - returns the JSON representation of an object

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+'''
+file: 5-to_json_string.py
+functions:
+-> to_json_string
+'''
+
+
 def to_json_string(my_obj):
+    ''' returns the JSON representation of an object '''
+
+    return json.dumps(my_obj)
