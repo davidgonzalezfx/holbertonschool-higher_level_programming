82e0ef3e7ff70886bf2583fce165dd329108f45c
davidgonzalezfx <dvdramos16@gmail.com>
2020-06-03 15:35:30 -0500

    Feature - retuns the dictionary description with simple data structure

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
-def append_after(filename="", search_string="", new_string=""):
+"""
+file: 10-class_to_json.py
+functions:
+-> class_to_json
+"""
+
+
+def class_to_json(obj):
+    """ retuns the dictionary description with simple data structure """
+
+    structure = {}
+    if hasattr(obj, "__dict__"):
+        structure = obj.__dict__.copy()
+    return structure
