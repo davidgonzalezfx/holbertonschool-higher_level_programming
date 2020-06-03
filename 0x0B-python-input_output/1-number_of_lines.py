45a355d9c24e60a1126fa09f66214b26c49b1cfe
davidgonzalezfx <dvdramos16@gmail.com>
2020-06-03 12:56:41 -0500

    Feature - number of lines of a text file

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
+"""
+file: 1-number_of_lines.py
+functions:
+-> number_of_lines
+"""
+
+
 def number_of_lines(filename=""):
+    """ returns the number of lines of a text file """
+
+    with open(filename) as file:
+        lines = file.readlines()
+    return len(lines)
