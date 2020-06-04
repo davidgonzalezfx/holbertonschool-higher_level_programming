b6d4d57c79479dad4ae9c8149ec7258a44972863
davidgonzalezfx <dvdramos16@gmail.com>
2020-06-03 20:50:39 -0500

    Feature - Advanced task 1

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+'''
+file: 100-append_after.py
+functions:
+-> append_after
+'''
+
+
+def append_after(filename="", search_string="", new_string=""):
+    '''inserts new_string to a file, after find containing search_string'''
+    with open(filename, "r+") as file:
+        lines = file.readlines()
+        changed = ''
+        for line in lines:
+            changed += line
+            if search_string in lines[line]:
+                changed += new_string
+
+        file.seek(0)
+        file.write(changed)
