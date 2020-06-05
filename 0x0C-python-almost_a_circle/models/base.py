bc51101c33dbce9c98e4f5e5603c6dffe1d8002f
davidgonzalezfx <dvdramos16@gmail.com>
2020-06-05 11:31:26 -0500

    Feature - 1. Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+'''
+file: base..py
+classes:
+-> Base
+'''
+
+
+class Base:
+    ''' Base class manage id attribute in all your future classes '''
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
