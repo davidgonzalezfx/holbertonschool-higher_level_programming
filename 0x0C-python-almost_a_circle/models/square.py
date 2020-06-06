703059f8648914c6ceaeac059ea6bddd5a914eed
davidgonzalezfx <dvdramos16@gmail.com>
2020-06-05 20:52:19 -0500

    Feature - 11. Square size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,11 +5,26 @@ classes:
 -> Square
 '''
 from models.rectangle import Rectangle
+
+
 class Square(Rectangle):
     ''' Square model, inherits from Rectangle '''
+
     def __init__(self, size, x=0, y=0, id=None):
         ''' Constructor method '''
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        ''' size getter '''
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ size setter """
+        self.width = value
+        self.height = value
+
     def __str__(self):
         ''' [Rectangle] (<id>) <x>/<y> - <width>/<height> '''
         txt = '[Square] ({}) {}/{} - {}'.format(
