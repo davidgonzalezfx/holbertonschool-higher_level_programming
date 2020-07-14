28a687a84aeebad7916aae9530452a4e55343bb3
davidgonzalezfx <dvdramos16@gmail.com>
2020-07-14 21:53:52 +0000

    Fix - Typo

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,8 @@
 -- States table
 -- creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USER hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-    id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT;
-    name VARCHAR(256) NOT NULL;
+    id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL
 );
