f435557fa8b2aba31752bbe455f3df9581bbb667
davidgonzalezfx <dvdramos16@gmail.com>
2020-07-14 22:23:51 +0000

    Feature - Genre ID by show

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+--  Genre ID by show
+--  lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id -- Query to join cities and states
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
