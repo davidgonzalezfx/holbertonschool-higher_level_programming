99d6b7e893cf338aae54fe68c9ff7dd1338d4807
davidgonzalezfx <dvdramos16@gmail.com>
2020-08-09 00:32:21 +0000

    Fix - PEP8

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
         'SELECT cities.id, cities.name, states.name FROM cities JOIN states ON\
         cities.state_id = states.id;')
 
-    states = cur.fetchall()
+    states = cursor.fetchall()
 
     for state in states:
         print(state)
