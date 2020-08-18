0cbdbf9d7f29aa4d285d5776b5cad2903191677f
davidgonzalezfx <dvdramos16@gmail.com>
2020-08-18 13:50:12 -0500

    Feature - Find peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+''' find peak '''
+
+
+def find_peak(list_of_integers):
+    if list_of_integers:
+        list_of_integers.sort()
+        return (list_of_integers[-1])
+    else:
+        return None
