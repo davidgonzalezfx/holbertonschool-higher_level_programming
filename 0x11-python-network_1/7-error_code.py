d4973a818ae0425291e10e335ac486989f5ebcb6
davidgonzalezfx <dvdramos16@gmail.com>
2020-08-24 10:35:02 -0500

    Feature - HTTP errors #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+''' HTTP errors '''
+import requests
+import sys
+
+if __name__ == '__main__':
+    req requests.get(sys.argv[1])
+    if req.status_code > 400:
+        print('Error code:', req.status_code)
+    else:
+        print(req.text)
