92cc13ba94e3395a932ac35eb3e6b5c5f54ecb2f
davidgonzalezfx <dvdramos16@gmail.com>
2020-08-24 10:21:36 -0500

    Feature - POST #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+''' POST request to the passed URL with the email '''
+import requests
+import sys
+
+if __name__ == '__main__':
+    r = requests.post(sys.argv[1], {'email': sys.argv[2]})
+    print(r.text)
