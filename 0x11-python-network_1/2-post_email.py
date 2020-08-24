a2d89a409b92d00bd3a1f5f210049489abc469f5
davidgonzalezfx <dvdramos16@gmail.com>
2020-08-24 09:53:40 -0500

    Feature - POST request

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+''' POST request to the passed URL with the email '''
+from urllib import request, parse
+import sys
+
+
+if __name__ == '__main__':
+    data = parse.urlencode({'email': sys.argv[2]})
+    data = data.encode('ascii')
+    req = request.Request(sys.argv[1], data)
+    with request.urlopen(req) as response:
+        body = response.read()
+        print(body.decode('utf-8'))
