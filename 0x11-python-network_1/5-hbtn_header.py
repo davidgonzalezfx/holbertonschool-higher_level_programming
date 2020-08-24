692b3eae0bb896e44465cf7e9f88ff5ccd8b63d7
davidgonzalezfx <dvdramos16@gmail.com>
2020-08-24 10:02:03 -0500

    Feature - Headers with requests

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+''' requests to get header info '''
+import requests
+import sys
+
+if __name__ == '__main__':
+    response = requests.get(sys.argv[1])
+    print(response.headers.get('X-Request-Id'))
