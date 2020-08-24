97b78aabb5c08e97d847080231f8ad3a223c11f6
davidgonzalezfx <dvdramos16@gmail.com>
2020-08-24 15:15:16 -0500

    Fix - Typo

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -4,7 +4,7 @@ import requests
 import sys
 
 if __name__ == '__main__':
-    req requests.get(sys.argv[1])
+    req = requests.get(sys.argv[1])
     if req.status_code > 400:
         print('Error code:', req.status_code)
     else:
