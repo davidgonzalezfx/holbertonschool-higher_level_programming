2123990823a028083a6a42caf0afa989d6a6b87a
davidgonzalezfx <dvdramos16@gmail.com>
2020-08-24 10:00:39 -0500

    Feature - requests package

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+''' requests package '''
+import requests
+
+
+if __name__ == "__main__":
+    req = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))
