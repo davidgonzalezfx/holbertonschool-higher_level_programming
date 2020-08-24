02950fbef48a4ed366ca63f9a24d2fc1039c9d6d
davidgonzalezfx <dvdramos16@gmail.com>
2020-08-24 11:00:29 -0500

    Feature - Github commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+''' Github commits '''
+from requests import get, auth
+import sys
+
+
+if __name__ == '__main__':
+    try:
+        repo = sys.argv[1]
+        owner = sys.argv[2]
+        url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+        res = get(url)
+        jsonify = res.json()
+        for i in range(0, 10):
+            print('{}: {}'.format(jsonify[i].get('sha'), jsonify[i].get(
+                'commit').get('author').get('name')))
+    except:
+        pass
