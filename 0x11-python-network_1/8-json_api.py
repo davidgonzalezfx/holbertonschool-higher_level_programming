1db6844ccb7f13bba26a818ea081bef77e6ad11a
davidgonzalezfx <dvdramos16@gmail.com>
2020-08-24 10:39:12 -0500

    Feature - Search API json

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+''' Search API '''
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    data = {'q': ''}
+
+    try:
+        data['q'] = sys.argv[1]
+    except:
+        pass
+
+    res = requests.post('http://0.0.0.0:5000/search_user', data)
+
+    try:
+        jsonify = res.json()
+        if not jsonify:
+            print('No result')
+        else:
+            print('[{}] {}'.format(jsonify.get('id'), jsonify.get('name')))
+    except:
+        print('Not a valid JSON')
