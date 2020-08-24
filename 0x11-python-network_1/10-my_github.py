bdaca27118a154c06708647f12b202989e184336
davidgonzalezfx <dvdramos16@gmail.com>
2020-08-24 10:48:14 -0500

    Feature - Github API

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+''' Github API '''
+from requests import get, auth
+import sys
+
+
+if __name__ == '__main__':
+    url = 'https://api.github.com/user'
+    user = sys.argv[1]
+    pwd = sys.argv[2]
+    res = get(url, auth=auth.HTTPBasicAuth(user, pwd))
+    print(res.json().get('id'))
