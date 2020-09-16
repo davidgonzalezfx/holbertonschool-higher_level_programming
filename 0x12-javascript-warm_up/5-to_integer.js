8d78f5e8c22ffdbd5c0b62edc22ec9954c7a4c5a
davidgonzalezfx <dvdramos16@gmail.com>
2020-09-15 19:41:50 -0500

    Fix - semi-standard

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const argv = process.argv;
 const num = parseInt(argv[2]);
 if (isNaN(num)) {
-	console.log('Not a number');
+  console.log('Not a number');
 } else {
-	console.log(`My number: ${num}`);
+  console.log(`My number: ${num}`);
 }
