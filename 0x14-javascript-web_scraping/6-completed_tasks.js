70c0c4ff3640c0bb8ada84e09a0e454c34b7f6e7
davidgonzalezfx <dvdramos16@gmail.com>
2020-09-22 15:43:23 -0500

    Feature - How many completed?

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const request = require('request');
+
+request(process.argv[2], function (err, response, body) {
+  if (err) throw err;
+  const users = {};
+  for (const task of JSON.parse(body)) {
+    if (task.completed) {
+      if (users[task.userId]) {
+        users[task.userId]++;
+      } else {
+        users[task.userId] = 1;
+      }
+    }
+  }
+  console.log(users);
+});
