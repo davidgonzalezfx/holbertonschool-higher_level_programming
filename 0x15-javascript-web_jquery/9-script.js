5c951282365d5768bef1b18ffde04ac53e148cdf
davidgonzalezfx <dvdramos16@gmail.com>
2020-09-24 12:47:19 -0500

    Feature - Javascript - Web JQuery

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,8 @@
+$(document).ready(() => {
+	$.get('https://fourtonfish.com/hellosalut/?lang=fr', (data, textStatus) => {
+	  if (textStatus === 'success') {
+		$('DIV#hello').text(data.hello);
+	  }
+	});
+  });
+  
